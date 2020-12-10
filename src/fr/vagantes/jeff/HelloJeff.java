35a38156e8bb281ae7152494186a8cae46b65b0e
DESKTOP-I0DAJ7O\propi <pro.pierre.nicolas@gmail.com>
2020-12-10 11:49:08 +0100

    [INIT] Create initial classes

---
+++ src/fr/vagantes/jeff/HelloJeff.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.vagantes.jeff;
+
+/**
+ * @author propi
+ *
+ */
+public class HelloJeff {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloJeff");
+	}
+
+}
